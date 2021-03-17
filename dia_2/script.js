28be7a5c27c9d81e131fafea224b097d965409d6
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-17 09:09:39 -0300

    Criando a Pasta do Bloco 6.2 e os exercicios

---
+++ dia_2/script.js
@@ -0,0 +1,109 @@
+function createStates(){
+    const states = ['AC - Acre',
+    'AL - Alagoas',
+    'AP - Amapá',
+    'AM - Amazonas',
+    'BA - Bahia',
+    'CE - Ceará',
+    'DF - Distrito Federal',
+    'ES - Espírito Santo',
+    'GO - Goías',
+    'MA - Maranhão',
+    'MT - Mato Grosso',
+    'MS - Mato Grosso do Sul',
+    'MG - Minas Gerais',
+    'PA - Pará',
+    'PB - Paraíba',
+    'PR - Paraná',
+    'PE - Pernambuco',
+    'PI - Piauí',
+    'RJ - Rio de Janeiro',
+    'RN - Rio Grande do Norte',
+    'RS - Rio Grande do Sul',
+    'RO - Rondônia',
+    'RR - Roraíma',
+    'SC - Santa Catarina',
+    'SP - São Paulo',
+    'SE - Sergipe',
+    'TO - Tocantins'
+    ];
+    const getSelect = document.querySelector('#estados');
+
+    for (let index = 0; index < states.length; index += 1) {
+        const statesArray = states[index];
+        const optionItem = document.createElement('option');
+        optionItem.innerHTML = statesArray;
+        optionItem.originalText = statesArray;
+
+        getSelect.appendChild(optionItem);
+    };
+};
+createStates();
+
+function ValidatingDate(data) {
+    if (data.indexOf('/') === 2 || data.indexOf('/') === 5) {
+     const dia = data.substr(0, 2);
+     console.log(dia);
+     const mes = data.substr(3, 2);
+     console.log(mes);
+     const ano = data.substr(6);
+     console.log(ano);
+
+     if ((dia > 0 && dia <= 31) && (mes > 0 && mes <= 12) && (ano > 0 && ano.length === 4)) {
+        return true;
+     }
+     return false;
+    }
+};
+
+function AnalyzingDate() {
+    const getData = document.querySelector('#data-text');
+    let data = getData.value;
+    const userData = ValidatingDate(data);
+  
+    if (!userData && data.length) {
+        getData.value = '';
+        alert('data invalida');
+        return false;
+    }
+    return userData;
+  };
+
+// Requisito 3 - Interropendo o fluxo automático do Form
+function checkingForms(event) {
+  event.preventDefault();
+  const getForms = document.querySelectorAll('input');
+
+  for (let index = 0; index < getForms.length; index += 1) {
+    if (getForms[index].type === 'radio' && !getForms[index].checked) {
+        continue;
+    }
+    const userForms = getForms[index].value;
+    const userData = document.querySelector('.formulario');
+    if (AnalyzingDate()) {
+        const div = document.createElement('div');
+        div.className = 'div-forms';
+        div.innerHTML = userForms;
+        userData.appendChild(div);
+    }
+  }
+}
+
+//Requisito 4
+function clearForms() {
+    const userForms = document.querySelectorAll('input');
+    const textArea = document.querySelector('#curriculo');
+    const div = document.querySelectorAll('.div-forms');
+
+    for (let index = 0; index < userForms.length && index < div.length; index += 1) {
+       const inputForms = userForms[index];
+       inputForms.value = '';
+       textArea.value = '';
+       div[index].innerText = '';
+    }
+}
+
+const btnSubmit = document.querySelector('.btn-envio');
+const btnClear = document.querySelector('.btn-limpar');
+btnSubmit.addEventListener('click',checkingForms);
+btnClear.addEventListener('click', clearForms);
